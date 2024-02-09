3d232c47b29f39bcb2c0d7794d39f269cf222972
“诸镇辉” <zhuzhu200902@163.com>
2024-02-09 13:47:02 +0800

    Added an important comment

---
+++ src/main/java/net/potatoes_dream/tutorialmod/TutorialMod.java
@@ -0,0 +1,38 @@
+package net.potatoes_dream.tutorialmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod {
+    public static final String MOD_ID = "tutorialmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public TutorialMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    //Very Important Comment
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents{
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+        }
+    }
+}
